7a2360a1683b5b2d47b069f6d9983702f41aff69
brehovkirill6-afk <brehovkirill6@gmail.com>
2026-01-20 21:48:14 +0300

    .github/workflows/build.yml

---
+++ src/main/java/com/arcanacraft/ArcanaCraft.java
@@ -0,0 +1,13 @@
+
+package com.arcanacraft;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("arcanacraft")
+public class ArcanaCraft {
+    public static final String MODID = "arcanacraft";
+
+    public ArcanaCraft() {
+        System.out.println("ArcanaCraft loaded");
+    }
+}
